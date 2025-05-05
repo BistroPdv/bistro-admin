dc6a8b1df118c005409338a08fb35bf09371b93b
diegcarlos <diegcarlos@gmail.com>
2025-05-05 06:49:49 -0400

    feat(users): adiciona layout e formulário de gerenciamento de usuários
    - Cria o componente MainLayout para a página de usuários, definindo o título da página.
    - Implementa o componente UserList, que exibe uma lista de usuários com opções de edição e exclusão.
    - Adiciona o UserFormModal para cadastro e edição de usuários, incluindo validação de formulário com Zod.
    - Integra lógica para manipulação de usuários, permitindo adicionar, editar e remover usuários com feedback visual.

--- src/app/(Aplication)/users/page.tsx
+++ src/app/(Aplication)/users/page.tsx
@@ -1,14 +1,13 @@
+import { UserList } from "@/components/user-form";
 import { RiUserFollowLine } from "@remixicon/react";
-import type { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "Bistro Admin - Usuários",
-};
 
 export default function Page() {
   return (
-    <div className="text-2xl font-bold flex gap-2 items-center">
-      <RiUserFollowLine /> Usuários
+    <div className="space-y-6">
+      <div className="text-2xl font-bold flex gap-2 items-center">
+        <RiUserFollowLine /> Usuários
+      </div>
+      <UserList />
     </div>
   );
 }
