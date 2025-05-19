0fcb1a67d198aa4170e6c6d6a827f3da0f40808a
Diego Carlos <53383018+diegcarlos@users.noreply.github.com>
2025-05-19 08:07:37 -0400

    feat(config): ajusta cabeçalhos de segurança e remove arquivos SVG desnecessários
    - Desativa os cabeçalhos de segurança em ambiente de desenvolvimento no arquivo de configuração do Next.js, facilitando o desenvolvimento local.
    - Atualiza a política de Content-Security-Policy para incluir uma nova origem para connect-src, melhorando a segurança da aplicação.
    - Remove arquivos SVG não utilizados do diretório público, contribuindo para a limpeza do projeto.

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,11 @@ const nextConfig: NextConfig = {
     domains: ["s3.bistro.app.br"],
   },
   async headers() {
+    // Desativa headers de segurança em desenvolvimento
+    if (process.env.NEXT_PUBLIC_NODE_ENV === "development") {
+      return [];
+    }
+
     return [
       {
         source: "/:path*",
@@ -33,7 +38,7 @@ const nextConfig: NextConfig = {
           {
             key: "Content-Security-Policy",
             value:
-              "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://accounts.google.com; style-src 'self' 'unsafe-inline'; img-src 'self' data: https://s3.bistro.app.br https://lh3.googleusercontent.com; font-src 'self' data:; connect-src 'self' https://accounts.google.com https://oauth2.googleapis.com https://www.googleapis.com https://api.bistro.app.br; frame-src 'self' https://accounts.google.com",
+              "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://accounts.google.com; style-src 'self' 'unsafe-inline'; img-src 'self' data: https://s3.bistro.app.br https://lh3.googleusercontent.com; font-src 'self' data:; connect-src 'self' https://accounts.google.com https://oauth2.googleapis.com https://www.googleapis.com https://api.bistro.app.br https://cdn.omie.com.br; frame-src 'self' https://accounts.google.com",
           },
         ],
       },
